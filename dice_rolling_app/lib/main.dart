6d51daedf8574d07f128704e8cb4eb12f1e2b3b6
unknown <richardcastuera16@gmail.com>
2023-06-23 14:37:21 +0800

    feat(dice_rolling_app): Added images inside Expanded Widget

--- dice_rolling_app/lib/main.dart
+++ dice_rolling_app/lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
+          centerTitle: true,
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -16,8 +18,21 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+      ],
+    );
   }
 }
