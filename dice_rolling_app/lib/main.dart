e967a4e8d652ed95ca5b73b5f2d30ee3d9120706
unknown <richardcastuera16@gmail.com>
2023-06-23 16:12:22 +0800

    feat(dice_rolling_app): Utilized Random Function from Dart Math Library

--- dice_rolling_app/lib/main.dart
+++ dice_rolling_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -26,6 +27,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +39,7 @@ class _DicePageState extends State<DicePage> {
               onPressed: () {
                 setState(
                   () {
-                    leftDiceNumber = 2;
+                    leftDiceNumber = Random().nextInt(6) + 1;
                     print('diceNumber = $leftDiceNumber');
                   },
                 );
@@ -48,9 +50,14 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('right button got pressed.');
+                setState(
+                  () {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                    print('diceNumber = $rightDiceNumber');
+                  },
+                );
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
