e252c2a9eecc4c7bb278770aed150f938ad4f1f1
unknown <richardcastuera16@gmail.com>
2023-06-23 16:02:42 +0800

    task(dice_rolling_app): Modified Stateless to Statefull Widget and Add Set State

--- dice_rolling_app/lib/main.dart
+++ dice_rolling_app/lib/main.dart
@@ -17,18 +17,30 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 4;
-
     return Center(
       child: Row(
         children: [
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('left button got pressed.');
+                setState(
+                  () {
+                    leftDiceNumber = 2;
+                    print('diceNumber = $leftDiceNumber');
+                  },
+                );
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
